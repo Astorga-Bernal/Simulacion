dc3805b8b02cda182816d07350a1e0ea6077a44e
Matias-Bernal <payomaty666@gmail.com>
2015-04-08 19:46:10 -0300

    Update mm2-estructura.c

--- mm1-estructura.c
+++ mm1-estructura.c
@@ -29,8 +29,6 @@ float queue_examine_service_time (void);
 float queue_examine_arrive_system_time (void);
 float queue_examine_arrive_queue_time (void);
 
-float num_random; /*variable utilizada para almacenar numero random*/
-float num_random2;
 float sim_time;
 float num_custs_delayed;
 float num_delays_required;
@@ -60,31 +58,32 @@ float time_arrive;
 
 main()  /* Main function. */
 {
-
     /* Initialize the simulation. */
     initialize();
-
     /* Run the simulation while more delays are still needed. */
-  
+	
     /*Num.clientes atendidos <  Num.clientes requeridos*/
-    while (num_custs_delayed < num_delays_required) { 
-       
+    while ( sim_time < 100 ) { 
+		printf("TIEMPO: %2f \n", sim_time);
+		printf("PROXIMO ARRIBO EN: %2f \n", time_next_arrive);
+		printf("PROXIMA SALIDA EN: %2f \n", time_next_depart);
+		printf("-----------------------\n");
         /* Determine the next event. */
         timing();
         /* Update time-average statistical accumulators. */
-
         update_time_avg_stats();
-
         /* Invoke the appropriate event function. */
-
         switch (next_event_type) {
             case 1:
+				printf("ARRIBO EN: %2f \n", time_next_arrive);
                 arrive();
                 break;
             case 2:
-                depart();
+                printf("SALIDA EN: %2f \n", time_next_depart);
+				depart();
                 break;
         }
+		printf("-----------------------\n");
     }
     /* Invoke the report generator and end the simulation. */
     report();
@@ -123,23 +122,15 @@ void initialize(void)  /* Initialization function. */ /*--inicializacion de las 
     /* (service completion) event is eliminated from consideration. */
     /*arreglos donde se guarda en el primer elemento el tiempo de la proximo arribo y en el segundo */
     /*elemento el tiempo de la proxima salida --se podria hacer con 2 variables --*/
-    time_next_arrive = sim_time + gen_next_interarrive() ; /* Planificacion del primer arribo */
+    time_next_arrive = gen_next_interarrive() ; /* Planificacion del primer arribo */
     time_next_depart = FLT_MAX; /* infinito --si tengo multiples servidores deberia tener una lista del mismo # de servidores para la salida */
 }
 
-
 void timing(void)  /* Timing function. */ /*--Se fija el minimo entre el proximo tiempo de entrada y el proximo tiempo de salida--*/
 {
-    int   i;
-    float min_time_next_event = FLT_MAX;
-
-    next_event_type = 0; /* Al final debe quedar en 1 en caso de que el proximo evento sea un arribo, 2: si es la salida de un servicio */
-
-    /* Determine the event type of the next event to occur. */
-    next_event_type = min(time_next_arrive,time_next_depart);
-
+    next_event_type = min(time_next_arrive,time_next_depart); /* Al final debe quedar en 1 en caso de que el proximo evento sea un arribo, 2: si es la salida de un servicio */
     /* advance the simulation clock. */
-    if(next_event_type == 1){
+    if( next_event_type == 1){
         sim_time = time_next_arrive;
     }else{
         sim_time = time_next_depart;
@@ -149,10 +140,7 @@ void timing(void)  /* Timing function. */ /*--Se fija el minimo entre el proximo
 
 void arrive(void)  /* Arrival event function. */
 {
-    float delay;
-
     /* Schedule next arrival. */
-
     time_next_arrive = sim_time + gen_next_interarrive();
 	num_customers_in_system += 1;
     /* Check to see whether server is busy. */
@@ -163,7 +151,7 @@ void arrive(void)  /* Arrival event function. */
         /* Guardar el tiempo de arribo de esta entidad para los calculos estadisticos */ 
         /* es para calcular el tiempo que espero en cola*/
 		num_in_q += 1;
-        queue_add(gen_next_service_time());
+        queue_add(sim_time+gen_next_service_time());
     }
     else {
 
@@ -174,16 +162,12 @@ void arrive(void)  /* Arrival event function. */
 		
         /* Schedule a departure (service completion). */
 		
-        time_next_depart = sim_time + gen_next_service_time(); /* generar con una funcion (con una distribucion estadistica) el tiempo de servicio */
+        time_next_depart = sim_time+gen_next_service_time(); /* generar con una funcion (con una distribucion estadistica) el tiempo de servicio */
     }
 }
 
-
 void depart(void)  /* Departure event function. */
 {
-    int   i;
-    float delay;
-
     /* Check to see whether the queue is empty. */
 	num_servers_in_use -= 1;
 	sum_time_customers_in_system += (sim_time-time_arrive);
@@ -193,6 +177,7 @@ void depart(void)  /* Departure event function. */
         /* The queue is empty so make the server idle and eliminate the */
         /* departure (service completion) event from consideration. */
         server_status = IDLE;
+		time_next_depart = FLT_MAX;
     }
     else {
 
@@ -201,11 +186,12 @@ void depart(void)  /* Departure event function. */
 
         /* Compute the delay of the customer who is beginning service and update
            the total delay accumulator. */
+		time_next_depart = sim_time + queue_examine_service_time() ;
 		time_arrive = queue_examine_arrive_system_time();
         queue_remove();     
         /* Increment the number of customers delayed, and schedule departure. */
-        num_custs_delayed += 1;
     }
+	num_custs_delayed += 1;
 }
 
 
@@ -227,7 +213,7 @@ void report(void)  /* Report generator function. */
     printf("Numero promedio cliente en sistema: %2f \n", sum_num_customers_in_system/sim_time );
 	
     /*Server utilization*/
-    printf("Utilizacion del servidor: %2f %\n", (area_server_status/sim_time)*100);
+    printf("Utilizacion del servidor: %2f \% \n", (area_server_status/sim_time)*100);
 }
 
 
@@ -256,22 +242,19 @@ float gen_next_interarrive()
     me retorna como resultado un interarrivo dependiendo del num_random generado.
     Aclaración: No fue posible realizar un switch por que no trabaja con tipo float.
     */
-	time_t tempo_segundos;
-
-	tempo_segundos = time ( NULL );
-   num_random = 0.0; /*inicialización de variable*/
-   num_random = lcgrand(1); /*generacion de random*/
-
-   /*Casos segun tabla del Ejercicio 3 - Pract. 1*/
-   if( 0.0 <= num_random && num_random <= 0.2 ){return 15.0;}
-   else if( 0.2 < num_random && num_random <= 0.35){return 20.0;}
-   else if( 0.35 < num_random && num_random <= 0.55 ){return 25.0;}
-   else if (0.55 < num_random && num_random <= 0.7){return 30.0;}
-   else if(0.7 < num_random && num_random <= 1){return 35.0;}
-   else if (num_random > 1.0){
-        exit(2); 
-        printf("Error!, pues las probabilidades van de 0 a 1 \n");
-   }
+	float num_random; /*variable utilizada para almacenar numero random*/
+	num_random = 0.0; /*inicialización de variable*/
+	num_random = lcgrand(1); /*generacion de random*/
+	/*Casos segun tabla del Ejercicio 3 - Pract. 1*/
+	if( 0.0 <= num_random && num_random <= 0.2 ){return 15.0;}
+	else if( 0.2 < num_random && num_random <= 0.35){return 20.0;}
+	else if( 0.35 < num_random && num_random <= 0.55 ){return 25.0;}
+	else if (0.55 < num_random && num_random <= 0.7){return 30.0;}
+	else if(0.9 < num_random && num_random <= 1){return 35.0;}
+	else if (num_random > 1.0){
+		exit(2); 
+		printf("Error!, pues las probabilidades van de 0 a 1 \n");
+	 }
 }
 
 float gen_next_service_time()
@@ -279,15 +262,16 @@ float gen_next_service_time()
     /*
     Aclaración: No fue posible realizar un switch por que no trabaja con tipo float.
     */
-   num_random2 = 0.0; /*inicialización de variable*/
-   num_random2 = lcgrand(1); /*generacion de random*/
+	float num_random; /*variable utilizada para almacenar numero random*/
+	num_random = 0.0; /*inicialización de variable*/
+	num_random = lcgrand(1); /*generacion de random*/
 
    /*Casos segun tabla del Ejercicio 3 - Pract. 1*/
-   if( 0.0 <= num_random2 && num_random2 <= 0.2 ){return 10.0;}
-   else if( 0.2 < num_random2 && num_random2 <= 0.55){return 15.0;}
-   else if( 0.55 < num_random2 && num_random2 <= 0.7){return 20.0;}
-   else if (0.7 < num_random2 && num_random2 <= 1.0){return 25.0;}
-   else if (num_random2 > 1.0){
+   if( 0.0 <= num_random && num_random <= 0.2 ){return 10.0;}
+   else if( 0.2 < num_random && num_random <= 0.55){return 15.0;}
+   else if( 0.55 < num_random && num_random <= 0.7){return 20.0;}
+   else if (0.7 < num_random && num_random <= 1.0){return 25.0;}
+   else if (num_random > 1.0){
         exit(2); 
         printf("Error!, pues las probabilidades van de 0 a 1 \n");
    }
@@ -295,11 +279,10 @@ float gen_next_service_time()
 
 int min(float x, float y)
 {
-    if(x<=y)
-    {
+    if(x<=y){
        return 1;
     }else{
-       return 2;
+		return 2;
     }
 }
 
@@ -308,7 +291,7 @@ void queue_add (float service_time)
 	if (num_in_q > Q_LIMIT) {
 		exit(2); /* cuando no tengo mas lugar para encolar clientes termina la simulacion */
 	}else{
-		queue[num_in_q][0]= gen_next_service_time();
+		queue[num_in_q][0]= service_time;
 		queue[num_in_q][1]= sim_time;
 		queue[num_in_q][2]= sim_time;
     }
